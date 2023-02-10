cf6c07b42df042865bf52680b62bff7980ff8a09
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-10 16:41:52 +0100

    created tables

--- database/migrations/2023_02_10_143046_create_trains_table.php
+++ database/migrations/2023_02_10_143046_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->string('codice_treno');
+            $table->tinyInteger('numero_di_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
