9180636366d65958e350f3a2e899f83cb4f44976
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-10 16:41:40 +0100

    added compact

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
     //
     public function  index()
     {
-        return view('welcome');
+        $trains = Train::all();
+        return view('welcome', compact('trains'));
     }
 }
