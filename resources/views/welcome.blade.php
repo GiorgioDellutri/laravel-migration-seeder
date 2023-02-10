cd9eaae28c7adafb4022eea105d9861794a0f83d
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-10 17:38:52 +0100

    added container & row

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,22 +18,27 @@
 <body>
 
     <main class="bg-light">
-        @forelse ($trains as $train)
-            <div class="col-4 p-3">
-                <div class="card p-4">
-                    <h3>Compagnia ferroviaria: {{ $train->azienda }}</h3>
-                    <p>Stazione di partenza: {{ $train->stazione_di_partenza }}</p>
-                    <p>Stazione di arrivo: {{ $train->stazione_di_arrivo }}</p>
-                    <p>Orario di partenza: {{ $train->orario_di_partenza }}</p>
-                    <p>Orario di arrivo: {{ $train->orario_di_arrivo }}</p>
-                    <p>Codice treno: {{ $train->codice_treno }}</p>
-                    <p>Numero di carrozze: {{ $train->numero_di_carrozze }}</p>
-                    <p>Treno in orario: {{ $train->in_orario }}</p>
-                    <p>Cancellato: {{ $train->cancellato }}</p>
-                </div>
+        <div class="container">
+            <div class="row">
+                @forelse ($trains as $train)
+                    <div class="col-4 p-3 dflex ">
+                        <div class="card p-4">
+                            <h3>Compagnia ferroviaria: {{ $train->azienda }}</h3>
+                            <p>Stazione di partenza: {{ $train->stazione_di_partenza }}</p>
+                            <p>Stazione di arrivo: {{ $train->stazione_di_arrivo }}</p>
+                            <p>Orario di partenza: {{ $train->orario_di_partenza }}</p>
+                            <p>Orario di arrivo: {{ $train->orario_di_arrivo }}</p>
+                            <p>Codice treno: {{ $train->codice_treno }}</p>
+                            <p>Numero di carrozze: {{ $train->numero_di_carrozze }}</p>
+                            <p>Treno in orario: {{ $train->in_orario ? 'No' : 'Si' }}</p>
+                            <p>Cancellato: {{ $train->cancellato ? 'No' : 'Si' }}</p>
+                        </div>
+                    </div>
+                @empty
+                    <p>Non ci sono treni da mostrare</p>
+                @endforelse
             </div>
-        @empty
-        @endforelse
+        </div>
     </main>
 </body>
 
